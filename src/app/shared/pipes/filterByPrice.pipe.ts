92b1f132575f759f818ae2f251ccda59f1e0af4b
Vel MAr <velmar.annabell@gmail.com>
2023-08-03 12:33:18 +0300

    some changes

--- src/app/shared/pipes/filterByPrice.pipe.ts
+++ src/app/shared/pipes/filterByPrice.pipe.ts
@@ -9,7 +9,7 @@ export class FilterPipeByPrice implements PipeTransform {
     transform(value: Product[], args: any[]): any {
         const filterField = args[0];
         let filteredValue = []
-        console.log(filterField);
+        console.log(filterField,"filterField");
         
      
 
@@ -19,6 +19,9 @@ export class FilterPipeByPrice implements PipeTransform {
                case '50-99': filteredValue = value.filter(v=>v.price<100 && v.price>49 );  return filteredValue;
                case '100-149': filteredValue = value.filter(v=>v.price<150 && v.price>99); return filteredValue;
                case '150-199': filteredValue = value.filter(v=>v.price<200 && v.price>149); return filteredValue;
+               case 'more than 199': filteredValue = value.filter(v=>v.price>=200); return filteredValue;
+
+              
    
            }
            return value

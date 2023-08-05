f2479b39cb663109eaa052a992cb3dcef22f5f85
Vel MAr <velmar.annabell@gmail.com>
2023-08-05 19:03:25 +0300

    correct rating

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -7,16 +7,21 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
   @Input() maxRating = 5;
-  @Input() SelectedStar: number = 0
-  @Output() onRating: EventEmitter<number>=new EventEmitter<number>();
+  @Input() SelectedStar: number;
+  @Output() onRating: EventEmitter<number> = new EventEmitter<number>();
   previousSelection = 0
+  totalRatings = 0;
+  cumulativeRating = 0;
 
   maxRatingArr: any = [];
+
 
   constructor() { }
   HandleMouseEnter(index: number) {
     this.SelectedStar = index + 1
   }
+
+
 
   HandleMouseLeave() {
     if (this.previousSelection !== 0) {
@@ -26,14 +31,34 @@ export class RatingComponent implements OnInit {
     }
   }
 
-  Rating(index){
-    this.SelectedStar=index+1;
-    this.previousSelection=this.SelectedStar;
+  Rating(index) {
+    this.SelectedStar = index + 1;
+    this.previousSelection = this.SelectedStar;
+    this.totalRatings++;
+    this.cumulativeRating += this.SelectedStar;
+    this.calculateAverageRating();
+
     this.onRating.emit(this.SelectedStar);
   }
 
   ngOnInit(): void {
-    this.maxRatingArr = Array(this.maxRating).fill(0)
+   // this.maxRatingArr = Array(this.maxRating).fill(0);
+    this.calculateAverageRating();
   }
 
+  updateStars() {
+    for (let i = 0; i < this.SelectedStar; i++) {
+      this.maxRatingArr[i] = 1; // Set the value to 1 to indicate a filled star
+    }
+  }
+  calculateAverageRating() {
+    if (this.totalRatings === 0) {
+      this.maxRatingArr = Array(this.maxRating).fill(0);
+    } else {
+      const averageRating = this.cumulativeRating / this.totalRatings;
+      const roundedAverage = Math.round(averageRating * 2) / 2; // Round to nearest 0.5
+
+      this.maxRatingArr = Array(this.maxRating).fill(0).map((_, index) => (index < roundedAverage ? 1 : 0));
+    }
+  }
 }

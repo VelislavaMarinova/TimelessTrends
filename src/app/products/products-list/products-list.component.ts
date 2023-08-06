d8e1ef060927a8f734633bddddde02661aacef2c
Vel MAr <velmar.annabell@gmail.com>
2023-08-06 17:47:49 +0300

    correct number of products

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -65,7 +65,7 @@ export class ProductsListComponent implements OnInit {
         case 'sunglasses': sliceNumber = 28, this.categoryImage = "https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"; break
         case 'womens-jewellery': sliceNumber = 14, this.categoryImage = "https://images.pexels.com/photos/7436133/pexels-photo-7436133.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"; break
         case 'skincare': sliceNumber = 25, this.categoryImage = "https://images.pexels.com/photos/10999606/pexels-photo-10999606.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"; break
-        case 'mens-watches': sliceNumber = 6, this.categoryImage = 'https://images.pexels.com/photos/380782/pexels-photo-380782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'; break
+        case 'mens-watches': sliceNumber = 10, this.categoryImage = 'https://images.pexels.com/photos/380782/pexels-photo-380782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'; break
         case 'womens-bags': sliceNumber = 0, this.categoryImage = "https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"; break;
         case 'fragrances': sliceNumber = 18, this.categoryImage = "https://images.pexels.com/photos/672051/pexels-photo-672051.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       }
@@ -84,7 +84,7 @@ export class ProductsListComponent implements OnInit {
 
             this.products = this.products.slice(0, sliceNumber);
 
-            if (this.products.length > 9) {
+            if (this.products.length > 12) {
               this.noProductsInTheList = false;
               this.loadMore = true
               this.productsToLoad = this.products.slice(0, this.numProductsPerPage);
@@ -92,7 +92,7 @@ export class ProductsListComponent implements OnInit {
               this.numLoadedProducs = this.numLoadedProducs + this.numProductsPerPage
 
 
-            } else if (this.products.length <= 9 && this.products.length !== 0) {
+            } else if (this.products.length <= 12 && this.products.length !== 0) {
               this.productsToLoad = this.products.slice(0);
               this.numLoadedProducs = this.productsToLoad.length;
               this.loadMore = false;
